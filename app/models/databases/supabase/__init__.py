660943c2e53fb101f5f5561a0b254aeb4e1de7ca
nguyen  duy anh <ngduyanhece@gmail.com>
2023-10-06 09:51:53 +0700

    add api key crud

--- app/models/databases/supabase/__init__.py
+++ app/models/databases/supabase/__init__.py
@@ -1,3 +1,4 @@
+from app.models.databases.supabase.api_key_handler import ApiKeyHandler
 from app.models.databases.supabase.brains import Brain
 from app.models.databases.supabase.prompts import Prompts
 from app.models.databases.supabase.user_usage import UserUsage
@@ -5,5 +6,6 @@ from app.models.databases.supabase.user_usage import UserUsage
 __all__ = [
     "Brain",
     "UserUsage",
-    "Prompts"
+    "Prompts",
+    "ApiKeyHandler"
 ]
--- app/routers.py
+++ app/routers.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
 
-from app.routes import brain, prompt, user
+from app.routes import api_key, brain, prompt, user
 
 router = APIRouter()
 router.include_router(user.router, prefix="", tags=["User"])
 router.include_router(brain.router, prefix="", tags=["Brain"])
 router.include_router(prompt.router, prefix="", tags=["Prompt"])
+router.include_router(api_key.router, prefix="", tags=["API Key"])
