5ec5a41d960400831246c2fe39c77b377226e1bf
nguyen  duy anh <ngduyanhece@gmail.com>
2023-11-28 11:13:16 +0700

    Revert "add noti and fix authen"
    This reverts commit 7c5c812c474d50130a91a956f5e250233dc273ff.

--- app/routers.py
+++ app/routers.py
@@ -4,7 +4,6 @@ from app.modules.agent.controller import agent_routes
 from app.modules.api_key.controller import api_key_routes
 from app.modules.brain.controller import brain_routes, runtime_router
 from app.modules.chat.controller import chat_routes
-from app.modules.notification.controller import notification_routes
 from app.modules.prompt.controller import prompt_routes
 from app.modules.user.controller import user_routes
 
@@ -16,5 +15,3 @@ router.include_router(brain_routes.brain_router, tags=["Brain"])
 router.include_router(runtime_router.runtime_router, tags=["Runtime"])
 router.include_router(chat_routes.chat_router, tags=["Chat"])
 router.include_router(agent_routes.agent_router, tags=["Agent"])
-router.include_router(notification_routes.notification_router, 
-                      tags=["Notification"])
