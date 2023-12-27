3eb912379fa237887003ed041b56027506a08dd4
nguyen  duy anh <ngduyanhece@gmail.com>
2023-12-27 08:09:18 +0700

    add test message socket

--- ally/server.py
+++ ally/server.py
@@ -1,4 +1,5 @@
 
+import chainlit as cl
 from chainlit.config import config
 from chainlit.server import app
 
@@ -9,9 +10,16 @@ logger = get_logger(__name__)
 
 # prevent the chainlit server from opening a browser window
 
+port = 5050
+
 config.run.headless = True
 config.run.watch = True
-config.run.host = "0.0.0.0"
-config.run.port = 5050
+config.run.port = port
+
+@cl.on_message
+async def on_message(message: cl.Message):
+	await cl.Message(
+		content="you have successfully connected to the chainlit server this is a test message your message was: {message}".format(message=message.content),
+	).send()
 
 app.include_router(router, prefix="")
