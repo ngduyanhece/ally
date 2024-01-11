c7cf9bdb37bd7a2333ebc263724d10302c84e082
nguyen  duy anh <ngduyanhece@gmail.com>
2024-01-11 23:11:18 +0700

    add json element

--- ally/modules/chainlit/utils.py
+++ ally/modules/chainlit/utils.py
@@ -1,13 +1,23 @@
 import asyncio
-from typing import Dict
+import json
+from typing import ClassVar, Dict
 
 import chainlit as cl
+from chainlit.element import Element, ElementType
 from core.settings import settings
 from openai import AsyncOpenAI
 from openai.types.beta.threads import (MessageContentImageFile,
                                        MessageContentText, ThreadMessage)
+from pydantic.dataclasses import dataclass
 
 client = AsyncOpenAI(api_key=settings.openai_api_key)
+
+
+
+@dataclass
+class Json(Element):
+	"""Useful to send a json to the UI."""
+	type: ClassVar[ElementType] = "json"
 
 def isAsync(someFunc):
 	return asyncio.iscoroutinefunction(someFunc)
@@ -42,6 +52,23 @@ async def process_thread_message(
 				msg = message_references[id]
 				msg.content = content_message.text.value
 				await msg.update()
+			# check if the content is a json
+			elif content_message.text.value.startswith('{'):
+				try:
+					json_obj = DictToObject(json.loads(content_message.text.value))
+					message_references[id] = cl.Message(
+						author=author, content=str(json_obj),
+						elements=[
+							Json(name="json", content=json_obj)
+						]
+					)
+					await message_references[id].send()
+				except Exception as e:
+					print(e)
+					message_references[id] = cl.Message(
+						author=author, content=content_message.text.value
+					)
+					await message_references[id].send()
 			else:
 				message_references[id] = cl.Message(
 					author=author, content=content_message.text.value
